93b2a702dc87fc2d077d228946e577528ef3ba8a
kuhfisch <kuhfisch@gmail.com>
2019-06-28 11:24:35 +0200

    some audio control

--- Assets/Scripts/AudioControll.cs
+++ Assets/Scripts/AudioControll.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,47 +7,66 @@ public class AudioControll : MonoBehaviour
 {
 
     private GameObject player;
-    public GameObject obst;
+    //public GameObject obst;
     private Vector3 playerPosition;
     public Hv_heavy_AudioLib test;
     private float distanceX;
     private float distanceZ;
+
+    public Hv_heavy_AudioLib.Parameter zPosToAudio;
+
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.Find("Player");
-       test.SetFloatParameter(Hv_heavy_AudioLib.Parameter.Metro, 200);
-       test.SetFloatParameter(Hv_heavy_AudioLib.Parameter.Tonone, 68);
+       //test.SetFloatParameter(Hv_heavy_AudioLib.Parameter.Metro, 200);
+       //test.SetFloatParameter(Hv_heavy_AudioLib.Parameter.Tonone, 68);
        // test.SetFloatParameter(Hv_heavy_AudioLib.Parameter.Tontwo, 0);
-       test.SetFloatParameter(Hv_heavy_AudioLib.Parameter.Soundbar, 0.1f);
+       //test.SetFloatParameter(Hv_heavy_AudioLib.Parameter.Soundbar, 0.1f);
+
+        Debug.Log(zPosToAudio);
+        foreach(Hv_heavy_AudioLib.Parameter para in Enum.GetValues(typeof(Hv_heavy_AudioLib.Parameter)))
+        {
+            Debug.Log(para);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        distanceZ = Mathf.Abs(obst.transform.position.z - player.GetComponent<Transform>().position.z);
-        distanceX = Mathf.Abs(obst.transform.position.x - player.GetComponent<Transform>().position.x);
+        distanceZ = Mathf.Abs(transform.position.z - player.GetComponent<Transform>().position.z);
+        distanceX = Mathf.Abs(transform.position.x - player.GetComponent<Transform>().position.x);
 
-        Debug.Log("X Abstand" + distanceX);
-        Debug.Log("z Abstand" + distanceZ);
+        float distZFloat = distanceZ / 10; // 10 is the current z position where the obstacles start
+        Debug.Log(distZFloat);
 
-        if (distanceX <= 0.5f)
-        {
-            Debug.Log("hallo");
-            test.SetFloatParameter(Hv_heavy_AudioLib.Parameter.Soundbar, test.GetFloatParameter(Hv_heavy_AudioLib.Parameter.Soundbar) + 0.01f);
-            //test.SendFloatToReceiver("soundBar", test.GetFloatParameter(Hv_heavy_AudioLib.Parameter.Soundbar) + 0.1f);
-            Debug.Log("Soundbar" + test.GetFloatParameter(Hv_heavy_AudioLib.Parameter.Soundbar));
+        //Debug.Log("X Abstand" + distanceX);
+        //Debug.Log("z Abstand" + distanceZ);
 
-            test.SetFloatParameter(Hv_heavy_AudioLib.Parameter.Metro, test.GetFloatParameter(Hv_heavy_AudioLib.Parameter.Metro) - 1f);
-            Debug.Log("Metronom" + test.GetFloatParameter(Hv_heavy_AudioLib.Parameter.Metro));
+        //Debug.Log("Metro" + test.GetFloatParameter(Hv_heavy_AudioLib.Parameter.Metro));
+        
+        
+        test.SetFloatParameter(Hv_heavy_AudioLib.Parameter.Metro, distZFloat * 200);
 
-            test.SendFloatToReceiver("tonone", test.GetFloatParameter(Hv_heavy_AudioLib.Parameter.Tonone) + 1f);
-        }
-        else 
-        {
-            test.SendFloatToReceiver("soundBar", 0.0f);
 
-        }
+
+        //if (distanceX <= 0.5f)
+        //{
+        //    Debug.Log("hallo");
+        //    test.SetFloatParameter(Hv_heavy_AudioLib.Parameter.Soundbar, test.GetFloatParameter(Hv_heavy_AudioLib.Parameter.Soundbar) + 0.01f);
+        //    //test.SendFloatToReceiver("soundBar", test.GetFloatParameter(Hv_heavy_AudioLib.Parameter.Soundbar) + 0.1f);
+        //    Debug.Log("Soundbar" + test.GetFloatParameter(Hv_heavy_AudioLib.Parameter.Soundbar));
+
+        //    test.SetFloatParameter(Hv_heavy_AudioLib.Parameter.Metro, test.GetFloatParameter(Hv_heavy_AudioLib.Parameter.Metro) - 1f);
+        //    Debug.Log("Metronom" + test.GetFloatParameter(Hv_heavy_AudioLib.Parameter.Metro));
+
+        //    test.SendFloatToReceiver("tonone", test.GetFloatParameter(Hv_heavy_AudioLib.Parameter.Tonone) + 1f);
+        //}
+        //else 
+        //{
+        //    test.SendFloatToReceiver("soundBar", 0.0f);
+
+        //}
 
     }
 }
